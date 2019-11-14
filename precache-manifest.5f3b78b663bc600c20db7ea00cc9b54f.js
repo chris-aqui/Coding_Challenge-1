c72fcf772ae4e228fb0a44f4c5908da60a7ab5c0
Christine <aqui.christine@gmail.com>
2019-11-14 06:49:55 -0500

    Updates

---
+++ precache-manifest.5f3b78b663bc600c20db7ea00cc9b54f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269633e80829dc74c554953015727bc1",
+    "url": "/Coding_Challenge-1/index.html"
+  },
+  {
+    "revision": "536ddeb3552c3d64551c",
+    "url": "/Coding_Challenge-1/static/css/main.5d5f607a.chunk.css"
+  },
+  {
+    "revision": "bf419ea726bc79189a34",
+    "url": "/Coding_Challenge-1/static/js/2.15538818.chunk.js"
+  },
+  {
+    "revision": "536ddeb3552c3d64551c",
+    "url": "/Coding_Challenge-1/static/js/main.43a75b41.chunk.js"
+  },
+  {
+    "revision": "cdd499d0810f5d8d7381",
+    "url": "/Coding_Challenge-1/static/js/runtime-main.2c899f98.js"
+  }
+]);
