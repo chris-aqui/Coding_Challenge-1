8384ea0ba5907de48cdf74ea0332691b557ab6cd
Christine <aqui.christine@gmail.com>
2019-11-13 22:14:43 -0500

    work on files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Title } from './components/Title';
+// styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Title />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./title.scss";
+
+function Title() {
+  return (
+		<div className="flex-container">
+			<div className="title container">
+				<div className="box box1"></div>
+				<div className="box box2"></div>
+			</div>
+			<h1>Blockchain</h1>
+		</div>
+	);
+}
+
+export default Title;
